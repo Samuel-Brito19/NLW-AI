e0bb96518ecfe91ca871dfcc2245c5c12f901b4e
Samuel Brito <sambriton7@gmail.com>
2023-09-16 18:36:19 -0300

    Adding a AI package

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,29 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from ".
 import { Slider } from "./components/ui/slider";
 import { VideoInputForm } from "./components/video-input-form";
 import { PromptSelect } from "./components/prompt-select";
+import { useState } from 'react'
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5)
+  const [videoId, setVideoId] = useState<string | null>(null)
+
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
+  } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -16,14 +37,16 @@ export function App() {
         <h1 className="text-xl font-bold">upload.ai</h1>
 
         <div className="flex items-center gap-3">
-          <span className="text-sm text-muted-foreground">Desenvolvido com ❤ no NLW da Rockeatseat</span>
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 💜 no NLW da Rocketseat
+          </span>
 
           <Separator orientation="vertical" className="h-6" />
 
-          <Button variant={"outline"}>
+          <Button variant="outline">
             <Github className="w-4 h-4 mr-2" />
-            Github</Button>
-
+            GitHub
+          </Button>
         </div>
       </div>
 
@@ -31,29 +54,38 @@ export function App() {
         <div className="flex flex-col flex-1 gap-4">
           <div className="grid grid-rows-2 gap-4 flex-1">
             <Textarea
-              className="resize-none p-5 leading-relaxed"
-              placeholder="Inclua o prompt para a IA" />
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+              value={input}
+              onChange={handleInputChange}
+            />
             <Textarea
-              className="resize-none p-5 leading-relaxed"
-              placeholder="Resultado gerado pela IA" readOnly />
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Resultado gerado pela IA"
+              readOnly
+              value={completion}
+            />
           </div>
-          <p className="text-sm text-muted-foreground">
-            Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.</p>
-        </div>
-        <aside className="w-60 space-y-6">
 
-          <VideoInputForm />
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável <code className="text-violet-400">{`{transcription}`}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
+          </p>
+        </div>
+
+        <aside className="w-80 space-y-6">
+          <VideoInputForm onVideoUploaded={setVideoId} />
 
           <Separator />
 
-          <form className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             <div className="space-y-2">
               <Label>Prompt</Label>
-              <PromptSelect />
+              <PromptSelect onPromptSelected={setInput} />
             </div>
+
             <div className="space-y-2">
               <Label>Modelo</Label>
-              <Select defaultValue="gpt3.5">
+              <Select disabled defaultValue="gpt3.5">
                 <SelectTrigger>
                   <SelectValue />
                 </SelectTrigger>
@@ -61,35 +93,37 @@ export function App() {
                   <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
                 </SelectContent>
               </Select>
-              <span className="block text-xs text-muted-foreground italic"
-              >Você poderá customizar essa opção em breve</span>
+              <span className="block text-sm text-muted-foreground italic">
+                Você poderá customizar essa opção em breve
+              </span>
             </div>
 
             <Separator />
-            <div className="space-y-2">
+
+            <div className="space-y-4">
               <Label>Temperatura</Label>
               <Slider
                 min={0}
                 max={1}
                 step={0.1}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
               />
-              <span className="block text-xs text-muted-foreground italic leading-relaxed"
-              >Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros</span>
+              <span className="block text-sm text-muted-foreground italic leading-relaxed">
+                Valores mais altor tendem a deixar o resultado mais criativo e com possíveis erros.
+              </span>
             </div>
 
             <Separator />
 
-            <Button type="submit" className="w-full">
+            <Button disabled={isLoading} type="submit" className="w-full">
               Executar
               <Wand2 className="w-4 h-4 ml-2" />
             </Button>
-
           </form>
         </aside>
-
       </main>
     </div>
   )
 }
-
 
